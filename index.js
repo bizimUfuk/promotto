13c31846fe399f374bcf4a3cc1494817e229513e
Fkm <fkm@email.com>
2018-03-27 16:06:53 +0300

    pg update

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 var cool = require('cool-ascii-faces');
-var pg = require('pg');
+const { Pool } = require('pg');
+var pool = new Pool();
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -12,15 +13,16 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .get('/db', function (request, response){
-	pg.connect(process.env.DATABASE_URL, function(err, client, done){
+	console.log("hereeee");
+	pool.connect(process.env.DATABASE_URL, function(err, client, done){
 		client.query('SELECT * FROM test_table', function(err, result) {
-			done();
 			if(err){
 				console.error(err);
 				response.send("Error " + err);
 			}else{
 				response.render('pages/db', {results: result.rows});
-			}
+			};
+			done();
 		});
 	});
 })
