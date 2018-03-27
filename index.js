5b9b431e0c8bd6f20ad2b646b9d53b762baaa7c0
Fkm <fkm@email.com>
2018-03-27 15:37:12 +0300

    add /db

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 var cool = require('cool-ascii-faces');
-
+var pg = require('pg');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -11,4 +11,17 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
+  .get('/db', function (request, response){
+	pg.connect(process.env.DATABASE_URL, function(err, client, done){
+		client.query('SELECT * FROM test_table', function(err, result) {
+			done();
+			if(err){
+				console.error(err);
+				response.send("Error " + err);
+			}else{
+				response.render('pages/db', {results: result.rows});
+			}
+		});
+	});
+})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
