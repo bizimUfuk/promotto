9d7b0da148e7ff33ab6c8ffa768dac823208ae80
Fkm <fkm@email.com>
2018-04-04 13:35:46 +0300

    ipfs injected  in /db for test ppurpose

--- index.js
+++ index.js
@@ -36,6 +36,27 @@ express()
       }
       client.end();
     });
+//ipfs test from here
+// Start a disposable node, and get access to the api
+// print the node id, and stop the temporary daemon
+console.log("starting \n")
+const IPFSFactory = require('ipfsd-ctl')
+console.log("IPFSFactory defined \n")
+const f = IPFSFactory.create({ type: 'proc', exec: require('ipfs') })
+console.log("here ", f.version)
+console.log("here agan")
+f.spawn(function (err, ipfsd) {
+console.log("in f.spawn")
+  if (err) { throw err }
+  console.log("no error here")
+  ipfsd.api.id(function (err, id) {
+    if (err) { throw err }
+    console.log("soon id \n")
+    console.log(id)
+    ipfsd.stop()
+  })
+})
+//ipfs test end
   })
   .get('/ipfs/:hash', function (req, res){
     var hash = req.params['hash'];
