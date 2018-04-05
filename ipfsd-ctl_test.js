20695c28cd202e5bfd68d612c4df9a1b23bbde44
Fkm <fkm@email.com>
2018-04-05 16:54:45 +0300

    rollback

--- ipfsd-ctl_test.js
+++ ipfsd-ctl_test.js
@@ -1,19 +1,18 @@
-// Start a disposable node, and get access to the api
+// Start a remote disposable node, and get access to the api
 // print the node id, and stop the temporary daemon
-console.log("starting \n")
+
 const IPFSFactory = require('ipfsd-ctl')
-console.log("IPFSFactory defined \n")
-const f = IPFSFactory.create({port: 9090, type: 'proc', exec: require('ipfs') })
-console.log("here ", f.version)
-console.log("here agan")
-f.spawn(function (err, ipfsd) {
-console.log("in f.spawn")
+const f = IPFSFactory.create({port: 9091, type: 'proc', exec: require('ipfs')})
+
+f.spawn((err, ipfsd) => {
   if (err) { throw err }
-  console.log("no error here")
-  ipfsd.api.id(function (err, id) {
+
+  var node = ipfsd.api
+  node.files.cat("QmX3VWtJQn5EKSRHwzG9QX52NZs4sFyXDSWemsUcse3Ljs/index.html", 
+
+  function (err, res) {
     if (err) { throw err }
-    console.log("soon id \n")
-    console.log(id)
-    //ipfsd.stop()
+    console.log("res: ", res.toString('utf-8'))
   })
+
 })
