8b65e04c67ce26ee000e449ff9a8ea4120873924
Fkm <fkm@email.com>
2018-04-13 22:54:16 +0300

    ipfsd ctl test

--- ipfsd-ctl_test.js
+++ ipfsd-ctl_test.js
@@ -1,26 +1,50 @@
-// Start a remote disposable node, and get access to the api
-// print the node id, and stop the temporary daemon
-
 const IPFSFactory = require('ipfsd-ctl')
-const f = IPFSFactory.create({port: 9091, type: 'proc', exec: require('ipfs')})
+const f = IPFSFactory.create({type: 'proc', exec: require('ipfs') })
 
-f.spawn((err, ipfsd) => {
+f.spawn({ disposable: false , repoPath: "./.ipfsdctlrepo"},(err, ipfsd) => {
   if (err) { throw err }
 
-  var node = ipfsd.api
+console.log("IpfsRepo path: ", ipfsd.repo.path);
+
+    ipfsd.init({directory: ipfsd.repo.path},(_) => {
+      ipfsd.start((_) => {
+        ipfsd.api.id((_, id) => {
+          if (err) {
+            throw err
+          }
+          console.log('alice')
+          console.log(id)
+          //ipfsd.stop()
+        })
+      })
+    })
+})
+
+
+/*
+var node = ipfsd.api
 
 //get test
-  node.files.cat("QmX3VWtJQn5EKSRHwzG9QX52NZs4sFyXDSWemsUcse3Ljs/index.html", 
+  node.files.cat("QmRdKfihZKweeWKz4EWtJREHh8WjXqdiPp6beidSuXQQUG/index.html", 
   function (err, res) {
     if (err) { throw err }
     console.log("response to cat: ", res.toString('utf-8'))
-  })
-
 //Add test
   node.files.add(Buffer.from('hello'),
   function (err, res) {
     if (err) { throw err }
     var response = JSON.stringify(res);
     console.log("response to add: hash-  ", res[0].hash)
+	ipfsd.stop(() => {
+			console.log("Stopping Ipfsd instance");
+			console.log("IPFS Started 4: ", ipfsd.initialized);
+		})	
+  })	
+
   })
-})
+		
+		})
+	})
+
+*/
+
